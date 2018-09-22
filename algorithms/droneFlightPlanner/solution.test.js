113be3714cbb6a8eeb71301ea09e87b2918f6573
Allan James Lapid <ajLapid718@gmail.com>
2018-09-22 14:54:29 -0400

    Fix a typo

--- algorithms/droneFlightPlanner/solution.test.js
+++ algorithms/droneFlightPlanner/solution.test.js
@@ -5,37 +5,37 @@ describe("calcDroneMinEnergy(route)", function() {
 
   describe("examples", function() {
 
-    it("should return the sum of all leaf node values for example one", function() {
+    it("should return the minimum required energy for example one", function() {
       let route = [[0,1,19]];
       let actualValue = calcDroneMinEnergy(route);
       expect(actualValue).to.equal(0);
     });
 
-    it("should return the sum of all leaf node values for example two", function() {
+    it("should return the minimum required energy for example two", function() {
       let route = [[0,2,10],[10,10,8]];;
       let actualValue = calcDroneMinEnergy(route);
       expect(actualValue).to.equal(0);
     });
 
-    it("should return the sum of all leaf node values for example three", function() {
+    it("should return the minimum required energy for example three", function() {
       let route = [[0,2,6],[10,10,20]];
       let actualValue = calcDroneMinEnergy(route);
       expect(actualValue).to.equal(14);
     });
 
-    it("should return the sum of all leaf node values for example four", function() {
+    it("should return the minimum required energy for example four", function() {
       let route = [[0,2,10],[3,5,0],[9,20,6],[10,12,15],[10,10,8]];
       let actualValue = calcDroneMinEnergy(route);
       expect(actualValue).to.equal(5);
     });
 
-    it("should return the sum of all leaf node values for example five", function() {
+    it("should return the minimum required energy for example five", function() {
       let route = [[0,2,2],[3,5,38],[9,20,6],[10,12,15],[10,10,8]];
       let actualValue = calcDroneMinEnergy(route);
       expect(actualValue).to.equal(36);
     });
 
-    it("should return the sum of all leaf node values for example six", function() {
+    it("should return the minimum required energy for example six", function() {
       let route = [[0,2,10],[3,5,9],[9,20,6],[10,12,2],[10,10,10],[10,10,2]];
       let actualValue = calcDroneMinEnergy(route);
       expect(actualValue).to.equal(0);
